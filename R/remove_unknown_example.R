26c4dbbc8a9cbdf06f92160dd98cf555d1077f85
dmontecino <dmontecino@ucdavis.edu>
2024-01-07 20:37:39 -0300

    changes in files

--- R/remove_unknown_example.R
+++ R/remove_unknown_example.R
@@ -2,11 +2,8 @@
 #  Remove the rows where the PAs are unknown and also remove the example #
 # ---------------------------------------------------------------------- #
 
-dat_modified_filtered <- dat_modified_filtered %>% 
-  filter(protected_area!="ejemplo")
+# The respondent provided a person's name instead of the protected area name
+dat_modified<-dat_modified[-which(map_vec(dat_modified$protected_area, \(x) all(x$protected_area==c("Unknown protected area")))),]
 
-dat_modified_filtered_1 <- dat_modified_filtered %>% 
-  filter(protected_area!="Unknown protected area")
+dat_modified<-dat_modified[-which(map_vec(dat_modified$protected_area, \(x) all(x$protected_area==c("ejemplo")))),]
 
-
-# nrow(dat_modified_filtered_1)
