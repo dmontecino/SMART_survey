c4f499407eb072e272cb0bb0888ec1bda21e3269
dmontecino <dmontecino@ucdavis.edu>
2024-04-28 17:45:17 -0700

    updating files

--- R/load_packages.R
+++ R/load_packages.R
@@ -1,21 +1,11 @@
 library(readxl)
-# library(plyr)
-# library(tibble)
-library(tidyverse) # load the tidyverse library
-library(purrr) #for map functions
-library(lubridate) #for date functions
-library(skimr) #for summary statistics
-library(kableExtra) #for table formatting
-library(countrycode) #for country codes
-library(janitor) #for cleaning data
+library(tidyverse)
+library(purrr) 
+library(lubridate) 
+library(skimr) 
+library(kableExtra) 
+library(countrycode) 
+library(janitor) 
 library(english)
-# library(rnaturalearth)
-# library(rnaturalearthdata)
 library(stringr)
-# library(wdpar) 
-# library(foreach)
-# library(doFuture)
-# library(sf)
-# library(stringdist)
 library(stringi)
-# library(cowplot)
